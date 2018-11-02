94d53cd7997f6b93a9a819a7ad8aac4604678712
Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
2018-11-01 18:21:59 -0700

    x86/sgx: Linux Enclave Driver
    Intel Software Guard eXtensions (SGX) is a set of CPU instructions that
    can be used by applications to set aside private regions of code and
    data. The code outside the SGX hosted software entity is disallowed to
    access the memory inside the enclave enforced by the CPU. We call these
    entities as enclaves.
    
    This commit implements a driver that provides an ioctl API to construct
    and run enclaves. Enclaves are constructed from pages residing in
    reserved physical memory areas. The contents of these pages can only be
    accessed when they are mapped as part of an enclave, by a hardware
    thread running inside the enclave.
    
    The starting state of an enclave consists of a fixed measured set of
    pages that are copied to the EPC during the construction process by
    using ENCLS leaf functions and Software Enclave Control Structure (SECS)
    that defines the enclave properties.
    
    Enclave are constructed by using ENCLS leaf functions ECREATE, EADD and
    EINIT. ECREATE initializes SECS, EADD copies pages from system memory to
    the EPC and EINIT check a given signed measurement and moves the enclave
    into a state ready for execution.
    
    An initialized enclave can only be accessed through special Thread Control
    Structure (TCS) pages by using ENCLU (ring-3 only) leaf EENTER.  This leaf
    function converts a thread into enclave mode and continues the execution in
    the offset defined by the TCS provided to EENTER. An enclave is exited
    through syscall, exception, interrupts or by explicitly calling another
    ENCLU leaf EEXIT.
    
    Signed-off-by: Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Co-developed-by: Serge Ayoun <serge.ayoun@intel.com>
    Signed-off-by: Serge Ayoun <serge.ayoun@intel.com>
    Co-developed-by: Shay Katz-zamir <shay.katz-zamir@intel.com>
    Signed-off-by: Shay Katz-zamir <shay.katz-zamir@intel.com>
    Co-developed-by: Suresh Siddha <suresh.b.siddha@intel.com>
    Signed-off-by: Suresh Siddha <suresh.b.siddha@intel.com>

--- arch/x86/include/uapi/asm/sgx_errno.h
+++ arch/x86/include/uapi/asm/sgx_errno.h
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause */
+/* SPDX-License-Identifier: GPL-2.0 OR BSD-3-Clause WITH Linux-syscall-note */
 /*
  * Copyright(c) 2018 Intel Corporation.
  *
